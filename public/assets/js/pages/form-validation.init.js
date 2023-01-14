7de5b32a537570f45ee090ca8a5ca5e246e18af6
Semet <hamdanilombok@gmail.com>
2023-01-14 16:15:49 +0800

    add theme files

---
+++ public/assets/js/pages/form-validation.init.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".custom-validation").parsley()}),function(){"use strict";var t=document.querySelectorAll(".needs-validation");Array.prototype.slice.call(t).forEach(function(a){a.addEventListener("submit",function(t){a.checkValidity()||(t.preventDefault(),t.stopPropagation()),a.classList.add("was-validated")},!1)})}();
