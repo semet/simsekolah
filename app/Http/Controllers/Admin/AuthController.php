62eab4dee4171ff75acfbb904b66b39d55ed9568
Semet <hamdanilombok@gmail.com>
2023-01-14 16:11:52 +0800

    admin login functionality

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -9,6 +9,29 @@ class AuthController extends Controller
 {
     public function showLogin()
     {
-        return view();
+        return view('admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $login = auth()->guard('admin')
+            ->attempt([
+                'email' => $request->email,
+                'password' => $request->password,
+                'aktif' => 1
+            ]);
+        if($login) {
+            $request->session()->regenerate();
+            return redirect()->route('admin.home');
+        }else{
+            return back()->withErrors([
+                'email' => 'Kredensial tidal ditemukan.',
+            ])->onlyInput('email');
+        }
     }
 }
