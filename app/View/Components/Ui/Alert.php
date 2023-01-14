169a1fab6bc8fa82a67f91b387430b0e4b843496
Semet <hamdanilombok@gmail.com>
2023-01-14 16:13:44 +0800

    add alert component

---
+++ app/View/Components/Ui/Alert.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\View\Components\Ui;
+
+use Illuminate\View\Component;
+
+class Alert extends Component
+{
+    public $type;
+
+    public $message;
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($type, $message)
+    {
+        $this->type = $type;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.ui.alert');
+    }
+}
---
+++ resources/views/components/ui/alert.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-{{ $type }} bg-{{ $type }} text-white border-0 alert-dismissible fade show" role="alert">
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    {{ $message }}
+</div>
