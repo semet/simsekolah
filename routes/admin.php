e62558d14ec396237261b16386968c0cba39b0be
Semet <hamdanilombok@gmail.com>
2023-01-14 16:15:02 +0800

    admin routes file

--- routes/admin.php
+++ routes/admin.php
@@ -1,10 +1,19 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function() {
     Route::get('login', 'showLogin')
-        ->name('admin.login')
+        ->name('admin.login.show')
         ->middleware('guest:admin');
+    Route::post('login', 'login')->name('admin.login');
 });
+
+Route::middleware(['admin'])->group(function() {
+    //Home route
+    Route::get('home', [HomeController::class, 'index'])->name('admin.home');
+
+});
+
