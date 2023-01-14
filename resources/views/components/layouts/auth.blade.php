54b48d8b08df9559cf7326809a7cb3c4a15b1b86
Semet <hamdanilombok@gmail.com>
2023-01-14 16:13:23 +0800

    add Auth layout

---
+++ resources/views/components/layouts/auth.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <title>{{ $title }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Sistm Informasi Sekolah SIMSEKOLAH" name="description">
+        <meta content="Themesbrand" name="author">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+        <!-- Bootstrap Css -->
+        <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css">
+        <!-- Icons Css -->
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css">
+        <!-- App Css-->
+        <link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css">
+
+    </head>
+
+<body>
+
+    <div class="home-btn d-none d-sm-block">
+        <a href="index.html" class="text-dark"><i class="fas fa-home h2"></i></a>
+    </div>
+    <div class="account-pages my-5 pt-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4">
+                    {{-- Content --}}
+                    {{ $slot }}
+                    {{-- End Content --}}
+                    <div class="mt-5 text-center">
+                        <p class="mb-0">© Mollusca {{ now()->year }}</p>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+
+</body>
+
+</html>
