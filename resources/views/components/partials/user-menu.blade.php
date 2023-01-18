8ddd8115053cc3b587ec227feedd4239e757c88e
Semet <hamdanilombok@gmail.com>
2023-01-18 20:48:04 +0700

    update user menu

--- resources/views/components/partials/user-menu.blade.php
+++ resources/views/components/partials/user-menu.blade.php
@@ -11,7 +11,10 @@
         <a class="dropdown-item d-flex align-items-center" href="#"><i class="mdi mdi-cog font-size-17 align-middle me-1"></i> Settings<span class="badge bg-success ms-auto">11</span></a>
         <a class="dropdown-item" href="#"><i class="mdi mdi-lock-open-outline font-size-17 align-middle me-1"></i> Lock screen</a>
         <div class="dropdown-divider"></div>
-        <a class="dropdown-item text-danger" href="@auth('admin') {{ route('admin.logout') }} @endauth @auth('operator') {{ route('operator.logout') }} @endauth">
+        <a class="dropdown-item text-danger"
+            href="@auth('admin') {{ route('admin.logout') }}
+            @endauth @auth('operator') {{ route('operator.logout') }} @endauth
+            @auth('guru') {{ route('guru.logout') }} @endauth">
             <i class="bx bx-power-off font-size-17 align-middle me-1 text-danger"></i>
              Logout
         </a>
