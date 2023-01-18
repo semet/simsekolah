c3e51355363e7a34f5b300123afaace51f5d06f2
Semet <hamdanilombok@gmail.com>
2023-01-18 20:47:41 +0700

    Create Tahun Service

---
+++ app/Services/TahunService.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Services;
+
+use App\Models\Tahun;
+
+class TahunService {
+    /**
+     * Tahun Model
+     *
+     * @var App\Models\Tahun;
+     */
+    public $tahun;
+
+    public function __construct(Tahun $tahun)
+    {
+        $this->tahun = $tahun;
+    }
+    /**
+     * get current active year
+     *
+     * @return Illuminate\Database\Eloquent\Collection|null
+     */
+    public function getActiveId()
+    {
+        return $this->tahun->where('aktif', 1)->first();
+    }
+}
