69a53ef25657048211e28e879d928d03878773e3
Semet <hamdanilombok@gmail.com>
2023-01-18 10:18:27 +0700

    setup auth controller for guru

---
+++ app/Http/Controllers/Guru/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Guru;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('guru.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'nip' => 'required',
+            'password' => 'required'
+        ]);
+
+        $login = auth()->guard('operator')
+            ->attempt([
+                'nip' => $request->nip,
+                'password' => $request->password,
+                'aktif' => 1
+            ]);
+        if($login) {
+            $request->session()->regenerate();
+            return redirect()->route('guru.home');
+        }else{
+            return back()->withErrors([
+                'nip' => 'Kredensial tidal ditemukan.',
+            ])->onlyInput('nip');
+        }
+    }
+
+    public function logout()
+    {
+        auth()->guard('guru')->logout();
+        session()->regenerate();
+
+        return redirect()->route('guru.login.show');
+    }
+}
