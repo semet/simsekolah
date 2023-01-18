0011d5daab86cdc551b97879618e1a8596b1ab87
Semet <hamdanilombok@gmail.com>
2023-01-18 21:02:57 +0700

    create  semester service

---
+++ app/Services/SemesterService.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Services;
+
+use App\Models\Semester;
+
+class SemesterService {
+     /**
+     * Tahun Model
+     *
+     * @var App\Models\Semester;
+     */
+    public $semester;
+
+    public function __construct(Semester $semester)
+    {
+        $this->semester = $semester;
+    }
+    /**
+     * get current active year
+     *
+     * @return Illuminate\Database\Eloquent\Collection|null
+     */
+    public function getActiveId()
+    {
+        return $this->semester->where('aktif', 1)->first();
+    }
+}
