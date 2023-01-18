8d7f3692784cc556a7c3dc07cb1209a48882cb80
Semet <hamdanilombok@gmail.com>
2023-01-18 20:46:40 +0700

    create guru middleware

---
+++ app/Http/Middleware/GuruMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class GuruMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(! auth()->guard('guru')->check()){
+            return redirect()->route('guru.login.show');
+        }
+        return $next($request);
+    }
+}
