e42e8c282a5968033f04bf5704c6f7df03d68fad
Semet <hamdanilombok@gmail.com>
2023-01-18 20:48:25 +0700

    Add Tahun Seeder

--- database/seeders/TahunSeeder.php
+++ database/seeders/TahunSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tahun;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,28 @@ class TahunSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Tahun::createMany([
+            [
+                'nama' => '2021/2022',
+                'awal' => '2021-07-01',
+                'akhir' => '2022-06-30',
+                'aktif' => 0
+            ],
+            [
+                'nama' => '2022/2023',
+                'awal' => '2022-07-01',
+                'akhir' => '2023-06-30',
+                'aktif' => 0
+            ]
+        ])
+        ->each(function($tahun) {
+            $tahun->semester()->createMany([
+                [
+                    'nama' => 'Ganjil',
+                    'awal' => '2021-07-01',
+                    'akhir' => '2022-06-30',
+                ]
+            ]);
+        });
     }
 }
