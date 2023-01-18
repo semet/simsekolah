596eeafdfd97e214588d8bf8d4f3a0fac31d9073
Semet <hamdanilombok@gmail.com>
2023-01-18 20:47:22 +0700

    create guru sidebar

---
+++ resources/views/components/guru/sidebar.blade.php
@@ -0,0 +1,25 @@
+<ul class="metismenu list-unstyled" id="side-menu">
+    <li class="menu-title">Main</li>
+
+    <li>
+        <a href="{{ route('guru.home') }}" class="waves-effect">
+            <i class="fas fa-home"></i>
+            <span>Home</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{ route('guru.jadwal') }}" class="waves-effect">
+            <i class="fas fa-calendar-alt"></i>
+            <span>Jadwal</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="{{ route('guru.rapot') }}" class="waves-effect">
+            <i class="fas fa-book"></i>
+            <span>Rapot</span>
+        </a>
+    </li>
+
+</ul>
