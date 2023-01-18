44600d6cfa172a15f8263ee0ffe711690a5bb1af
Semet <hamdanilombok@gmail.com>
2023-01-18 20:47:55 +0700

    update selected

--- resources/views/components/input/select-day.blade.php
+++ resources/views/components/input/select-day.blade.php
@@ -1,9 +1,9 @@
 <select name="hari" id="hari" class="form-select">
     <option value="" selected>--Hari--</option>
-    <option value="senin">senin</option>
-    <option value="selasa">selasa</option>
-    <option value="rabu">rabu</option>
-    <option value="kamis">kamis</option>
-    <option value="jumat">jumat</option>
-    <option value="sabtu">sabtu</option>
+    <option @selected(request()->get('hari') == 'senin') value="senin">senin</option>
+    <option @selected(request()->get('hari') == 'selasa') value="selasa">selasa</option>
+    <option @selected(request()->get('hari') == 'rabu') value="rabu">rabu</option>
+    <option @selected(request()->get('hari') == 'kamis') value="kamis">kamis</option>
+    <option @selected(request()->get('hari') == 'jumat') value="jumat">jumat</option>
+    <option @selected(request()->get('hari') == 'sabtu') value="sabtu">sabtu</option>
 </select>
