cafddf04571c297c0a393b4f380ac1b742851d1a
Semet <hamdanilombok@gmail.com>
2023-01-18 10:19:22 +0700

    add guru to route middleware array

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'operator' => \App\Http\Middleware\OperatorMiddleware::class,
+        'guru' => \App\Http\Middleware\GuruMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
