52dce067d9125b6bd6134fb27c78cf3107ec0599
Semet <hamdanilombok@gmail.com>
2023-01-20 20:39:50 +0700

    creating auth controller for siswa

---
+++ app/Http/Controllers/Siswa/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Siswa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('siswa.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'nis' => 'required',
+            'password' => 'required'
+        ]);
+
+        $login = auth()->guard('siswa')
+            ->attempt([
+                'nis' => $request->nis,
+                'password' => $request->password,
+                'aktif' => 1
+            ]);
+        if($login) {
+            $request->session()->regenerate();
+            return redirect()->route('siswa.home');
+        }else{
+            return back()->withErrors([
+                'nip' => 'Kredensial tidal ditemukan.',
+            ])->onlyInput('nip');
+        }
+    }
+
+    public function logout()
+    {
+        auth()->guard('guru')->logout();
+        session()->regenerate();
+
+        return redirect()->route('guru.login.show');
+    }
+}
