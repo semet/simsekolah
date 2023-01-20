31b0398e8ea7e85d1549afe058d59818f94d202c
Semet <hamdanilombok@gmail.com>
2023-01-20 20:40:17 +0700

    create siswa sidebar

---
+++ resources/views/components/siswa/sidebar.blade.php
@@ -0,0 +1,11 @@
+<ul class="metismenu list-unstyled" id="side-menu">
+    <li class="menu-title">Main</li>
+
+    <li>
+        <a href="{{ route('siswa.home') }}" class="waves-effect">
+            <i class="fas fa-home"></i>
+            <span>Home</span>
+        </a>
+    </li>
+
+</ul>
