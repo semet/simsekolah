157bcb49c5aca0117364e78df1009954d88f3ffb
Semet <hamdanilombok@gmail.com>
2023-01-20 20:31:33 +0700

    add siswa middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,7 @@ class Kernel extends HttpKernel
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
         'operator' => \App\Http\Middleware\OperatorMiddleware::class,
         'guru' => \App\Http\Middleware\GuruMiddleware::class,
+        'siswa' => \App\Http\Middleware\SiswaMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
---
+++ app/Http/Middleware/SiswaMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class SiswaMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(! auth()->guard('siswa')->check()){
+            return redirect()->route('siswa.login.show');
+        }
+        return $next($request);
+    }
+}
