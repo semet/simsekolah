ba235b8457cc3a2dc9ce3122229c387506179ccf
Semet <hamdanilombok@gmail.com>
2023-01-24 23:15:26 +0700

    create dedicated route for kepsek

--- routes/kepsek.php
+++ routes/kepsek.php
@@ -3,4 +3,5 @@
 use App\Http\Controllers\Kepsek\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('email/verify/{id}', [RegisterController::class, 'verifyEmail'])->name('kepsek.email.verify');
+Route::get('email/verify/{id}', [RegisterController::class, 'verifyEmail'])
+    ->name('kepsek.email.verify');
