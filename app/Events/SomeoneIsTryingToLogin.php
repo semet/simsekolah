26ce4ae60ad82c8b89f242bbb191dedfa3c7be9f
Semet <hamdanilombok@gmail.com>
2023-01-25 06:11:49 +0700

    create OTP for login

---
+++ app/Events/SomeoneIsTryingToLogin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Admin;
+use App\Models\Guru;
+use App\Models\KepalaSekolah;
+use App\Models\Pegawai;
+use App\Models\Siswa;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SomeoneIsTryingToLogin
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Admin|KepalaSekolah|Guru|Pegawai|Siswa  $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
