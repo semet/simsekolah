f89da73cc8bba6d4e664dcaf416b74e2a6016943
Semet <hamdanilombok@gmail.com>
2023-01-24 09:52:39 +0700

    include edit component

--- resources/views/admin/semester/index.blade.php
+++ resources/views/admin/semester/index.blade.php
@@ -14,7 +14,6 @@
                 <x-ui.alert type="danger" message="{{ session('error') }}" />
             </div>
         @endif
-
         @if ($semester->isNotEmpty())
             <div class="table-responsive">
                 <table class="table table-striped mb-0">
@@ -34,16 +33,27 @@
                             <tr>
                                 <th scope="row">{{ $loop->iteration }}</th>
                                 <td>{{ $sm->nama }}</td>
-                                <td>{{ $sm->awal }}</td>
-                                <td>{{ $sm->akhir }}</td>
+                                <td>{{ \Carbon\Carbon::parse($sm->awal)->format('d, M Y') }}</td>
+                                <td>{{ \Carbon\Carbon::parse($sm->akhir)->format('d, M Y') }}</td>
                                 <td>
                                     @if($sm->aktif)
                                     <span class="badge bg-primary">Aktif</span>
                                     @else
-                                    <span class="badge bg-danger">Nonaktif</span>
+                                    <a href="{{ route('admin.semester.toggle', $sm->id) }}">
+                                        <span class="badge bg-danger">Nonaktif</span>
+                                    </a>
                                     @endif
                                 </td>
-                                <td>@mdo</td>
+                                <td>
+                                    <div class="btn-group btn-group-sm" role="group" aria-label="Tahun Options">
+                                        <button class="btn btn-info" data-bs-toggle="modal" data-bs-target="#editSemester" id="{{ $sm->id }}">
+                                            <i class="fas fa-edit"></i>
+                                        </button>
+                                        <button class="btn btn-danger" onclick="deleteSemester('{{ $sm->id }}')">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </div>
+                                </td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -61,12 +71,14 @@
     </x-ui.card>
 
     @push('styles')
-
+        <link href="{{ asset('assets/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
     @endpush
 
     @push('scripts')
+        <script src="{{ asset('assets/libs/sweetalert2/sweetalert2.min.js') }}"></script>
         <script src="{{ asset('assets/libs/parsleyjs/parsley.min.js') }}"></script>
         <script src="{{ asset('assets/js/pages/form-validation.init.js') }}"></script>
+        <script src="{{ asset('assets/libs/axios/axios.min.js') }}"></script>
         <script>
             $(document).ready(function () {
                 $('#tahun').change(function (e) {
@@ -76,10 +88,57 @@
                     }
                     $('#semesterFilterForm').submit();
                 });
+                //
+                $('#editSemester').on('show.bs.modal', function (e) {
+                    const id = e.relatedTarget.id;
+                    const url = '{{ route('admin.semester.edit', ':param') }}'
+                    const parsedUrl = url.replace(':param', id)
+
+                    axios.get(parsedUrl)
+                    .then(function (response) {
+                        $('#idEdit').val(response.data.semester.id);
+                        $('#tahunEdit').val(response.data.semester.tahun_id);
+                        $('#namaEdit').val(response.data.semester.nama);
+                        $('#awalEdit').val(response.data.semester.awal.slice(0, 10))
+                        $('#akhirEdit').val(response.data.semester.akhir.slice(0, 10))
+                        // console.log(new Date(response.data.semester.awal).toISOString().split('T')[0])
+                    })
+                })
             });
+            //Delete semester
+            function deleteSemester (id) {
+                Swal.fire({
+                    title: "Anda yakin?",
+                    text: "Proses ini tidak bisa dibatalkan!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#34c38f",
+                    cancelButtonColor: "#f46a6a",
+                    confirmButtonText: "Ya, hapus!"
+                }).then(function (result) {
+                    if (result.isConfirmed) {
+                        axios.post('{{ route('admin.semester.delete') }}', {
+                            _token: '{{ csrf_token() }}',
+                            id: id
+                        }).then(function(response) {
+                            if(response.status === 201) {
+                                Swal.fire("Berhasil!", response.data.message, "success")
+                                .then(function (result) {
+                                    if(result.isConfirmed) {
+                                        location.reload()
+                                    }
+                                });
+                            }
+                        })
+                    }
+                }).then(function(result) {
+                    console.log(result)
+                });
+            }
         </script>
     @endpush
-
+    {{-- Create --}}
     <x-admin.semester.create/>
-
+    {{-- Edit --}}
+    <x-admin.semester.edit/>
 </x-layouts.main>
